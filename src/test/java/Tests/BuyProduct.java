56fe96bbf57ac64085ccb405e5233fc669bcb4a0
nikoc <niko.chetilyazov@gmail.com>
2021-11-03 00:14:49 +0200

    Pages and locators

--- src/test/java/Tests/BuyProduct.java
+++ src/test/java/Tests/BuyProduct.java
@@ -1,105 +1,94 @@
 package Tests;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
+import Pages.Home;
+import Pages.Product;
+import Pages.Search;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class BuyProduct {
 
+    Home home;
+    Search search;
+    Product product;
+
     @Test
-    public void BuyProductTest() throws InterruptedException {
+    public void BuyProductTest() throws InterruptedException{
+
+        System.setProperty("webdriver.chrome.driver", "WebDriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://automationpractice.com/");
+        driver.manage().window().maximize();
+
         String email = "vase5555@gmail.com";
         String password = "Test@123";
         String quantity = "2";
         String size = "M";
+        String color = "Blue";
         String mediateTotalPrice;
         String finalTotalPrice;
-        int productsReturnedCount;
-        System.setProperty("webdriver.chrome.driver", "WebDriver/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+
+        home = new Home(driver);
+        search = new Search(driver);
+        product = new Product(driver);
 
         try {
-            driver.manage().window().maximize();
-            driver.get("http://automationpractice.com/");
-            driver.findElement(By.id("search_query_top")).sendKeys("Faded Short Sleeve T-shirts");
-            driver.findElement(By.xpath("//button[@name='submit_search']")).click();
-            Thread.sleep(5000);
-            productsReturnedCount = driver.findElements(By.xpath("//div[@class='product" +
-                    "-container" +
-                    "']")).size();
-            Assert.assertEquals(1, (int) productsReturnedCount);
-            driver.findElement(By.xpath("//div[@class='product-container']")).click();
-            Thread.sleep(5000);
+            home.enterSearchQuery("Faded Short Sleeve T-shirts");
+            home.clickSearch();
+            Assert.assertEquals(search.getResultsCount(), 1);
+            search.clickFirstProduct();
 
             //Quantity 2
-            WebElement quantityField = driver.findElement(By.id("quantity_wanted"));
-            quantityField.clear();
-            quantityField.sendKeys(quantity);
+            product.setProductQuantity(quantity);
 
             //Size M
-            driver.findElement(By.id("group_1")).click();
-            Select sizes = new Select(driver.findElement(By.name("group_1")));
-            sizes.selectByVisibleText(size);
+            product.selectSize(size);
 
             //Color Blue
-            driver.findElement(By.xpath("//a[@title='Blue']")).click();
-            Thread.sleep(5000);
+            product.selectColor(color);
 
             //Add to cart
-            driver.findElement(By.xpath("//button[@name='Submit']")).click();
-            WebDriverWait wait = new WebDriverWait(driver,3);
-            wait.until(ExpectedConditions.elementToBeClickable (By.xpath("//span[contains(text(), 'Proceed to checkout')]")));
+            product.addToCart();
 
             //Get Total price
-            mediateTotalPrice = driver.findElement(By.xpath("//span[@class='ajax_block_cart_total']")).getText();
-            System.out.println("MEDIATE PRICE IS: " + mediateTotalPrice);
+            Thread.sleep(2000);
+            mediateTotalPrice = product.getTotalPrice();
+            System.out.println("MEDIATE PRICE: " + mediateTotalPrice);
 
             //Proceed
-            Thread.sleep(2000);
-            driver.findElement(By.xpath("//span[contains(text(), 'Proceed to checkout')]")).click();
-            Thread.sleep(3000);
-            driver.findElement(By.xpath("//p[@class='cart_navigation clearfix']/a[@title='Proceed" +
-                    " to checkout']")).click();
+            product.clickProceed();
+            product.clickProceedInSummary();
 
             //Sign in with user
-            WebElement emailField = driver.findElement(By.id("email"));
-            emailField.clear();
-            emailField.sendKeys(email);
-
-            WebElement passwordField = driver.findElement(By.id("passwd"));
-            passwordField.clear();
-            passwordField.sendKeys(password);
-
-            driver.findElement(By.id("SubmitLogin")).click();
-            Thread.sleep(3000);
+            product.enterEmail(email);
+            product.enterPassword(password);
+            product.clickLoginButton();
 
             //Proceed
-            driver.findElement(By.xpath("//button[@name='processAddress']")).click();
-            Thread.sleep(3000);
+            product.clickProceedInAddress();
 
             //Agree TS
-            driver.findElement(By.id("cgv")).click();
+            product.agreeToTerms();
 
             //Proceed
-            driver.findElement(By.xpath("//button[@name='processCarrier']")).click();
+            product.clickProceedInShipping();
 
             //Pay by bank wire
-            driver.findElement(By.xpath("//a[@class='bankwire']")).click();
+            product.clickBankWirePayment();
 
             //Confirm
-            driver.findElement(By.xpath("//button[@type='submit']/span[contains(text(), 'I confirm my order')]")).click();
+            product.clickConfirmOrder();
 
-            //Assert completed
-            Assert.assertTrue(driver.findElement(By.xpath("//strong[contains(text(), 'Your order on My Store is complete.')]")).isDisplayed());
+            //Assert completion
+            Assert.assertTrue(product.isOrderConfirmed());
 
             //Assert amount
-            finalTotalPrice =  driver.findElement(By.xpath("//span[@class='price']")).getText();
+            finalTotalPrice = product.getCompletedTotalPrice();
             Assert.assertEquals(mediateTotalPrice, finalTotalPrice);
 
         } finally {
--- src/test/java/Tests/CheckPrice.java
+++ src/test/java/Tests/CheckPrice.java
@@ -1,43 +1,46 @@
 package Tests;
 
+import Pages.Home;
+import Pages.Search;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class CheckPrice {
 
-    @Test
-    public void CheckPriceTest() throws InterruptedException {
+    Home home;
+    Search search;
 
+    @Test
+    public void CheckPriceTest() {
+
+        //BeforeTest - testng maybe
         System.setProperty("webdriver.chrome.driver", "WebDriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://automationpractice.com/");
+        driver.manage().window().maximize();
+
+        home = new Home(driver);
+        search = new Search(driver);
 
         String stringExpectedPrice = "$16.51";
-        String stringActualPrice;
         double doubleExpectedPrice = 16.51;
         double doubleActualPrice;
-        int productsReturnedCount;
 
         try {
-            driver.manage().window().maximize();
-            driver.get("http://automationpractice.com/");
-            driver.findElement(By.id("search_query_top")).sendKeys("Faded Short Sleeve T-shirts");
-            driver.findElement(By.xpath("//button[@name='submit_search']")).click();
-            Thread.sleep(5000);
-            productsReturnedCount = driver.findElements(By.xpath("//div[@class='product" +
-                    "-container" +
-                    "']")).size();
-            Assert.assertEquals(1, (int) productsReturnedCount);
-            driver.findElement(By.xpath("//div[@class='product-container']")).click();
-            Thread.sleep(5000);
-            stringActualPrice =
-                    driver.findElement(By.xpath("//span[@id='our_price_display']")).getText();
-            //Assert stings
-            Assert.assertEquals(stringExpectedPrice, stringActualPrice);
-            //Assert doubles
-            doubleActualPrice = Double.parseDouble(stringActualPrice.replace("$",""));
+            home.enterSearchQuery("Faded Short Sleeve T-shirts");
+            home.clickSearch();
+            Assert.assertEquals(search.getResultsCount(), 1);
+            search.clickFirstProduct();
+
+            //Assert as strings
+            Assert.assertEquals(stringExpectedPrice, search.getPriceString());
+            //Assert as doubles
+            doubleActualPrice = Double.parseDouble(search.getPriceString().replace("$",""));
             Assert.assertEquals(doubleExpectedPrice, doubleActualPrice, 0.0);
 
         } finally {
